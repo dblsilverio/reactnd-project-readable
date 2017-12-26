754000da0726f8c6eb75d35b92467e9ec277dadb
Diogo Silvério <dblsilverio@gmail.com>
2017-12-25 22:30:51 -0200

    fix: Simplificando component Pontuacao

--- src/components/ui/Pontuacao.js
+++ src/components/ui/Pontuacao.js
@@ -2,28 +2,18 @@ import React, { Component } from 'react';
 
 export default class Pontuacao extends Component {
 
-    state = {
-        estado: 'badge badge-secondary'
-    }
+    render() {
 
-    componentWillReceiveProps(props) {
+        let estado = "badge badge-secondary";
 
-        let classeEstado = 'badge badge-secondary';
-
-        if (props.pontos > 0) {
-            classeEstado = 'badge badge-success'
-        } else if (props.pontos < 0) {
-            classeEstado = 'badge badge-danger'
+        if(this.props.pontos > 0){
+            estado = "badge badge-success";
+        } else if(this.props.pontos < 0){
+            estado = "badge badge-danger";
         }
 
-        this.setState({
-            estado: classeEstado
-        });
-    }
-
-    render() {
         return (
-            <span className={this.state.estado}>{this.props.pontos}</span>
+            <span className={estado}>{this.props.pontos}</span>
         );
     }
 

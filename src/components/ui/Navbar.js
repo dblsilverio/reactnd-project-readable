33f23365c418b7f7e1b5ed09039ce0c4100645d1
Diogo Silvério <dblsilverio@gmail.com>
2017-12-31 16:43:57 -0200

    refactoring: improving navbar ui

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import FABook from 'react-icons/lib/fa/book';
+import FANewspaper from 'react-icons/lib/fa/newspaper-o';
+
+
 import Client from '../../client/ReadAPI';
 
 export default class Navbar extends Component {
@@ -18,30 +22,23 @@ export default class Navbar extends Component {
     render() {
         return (
 
-            <nav className="navbar navbar-expand-md navbar-light bg-light mb-4">
-                <Link className="navbar-brand" to="/">Leitura</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarCollapse">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <Link className="nav-link" to="/">Home</Link>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <Link className="nav-link dropdown-toggle" to="" id="dropdown03" data-toggle="dropdown" aria-haspopup="true"
-                                aria-expanded="false">Categorias</Link>
-                            <div className="dropdown-menu" aria-labelledby="dropdown03">
-                                {this.state.categories.map(category => (
-                                    <Link className="dropdown-item" key={category.path} to={`/categoria/${category.name}`}>{category.name}</Link>
-                                ))}
-                            </div>
-                        </li>
-                    </ul>
-                    <span className="form-inline mt-2 mt-md-0">
-                        <Link className="btn btn-outline-primary my-2 my-sm-0" to="/novo">Nova Postagem</Link>
-                    </span>
+            <nav className="navbar navbar-expand-md bg-primary navbar-dark">
+                <div className="container">
+                    <Link className="navbar-brand" to="/"><FABook /><b>&nbsp;Leitura</b></Link>
+                    <Link className="btn btn-default navbar-btn btn-primary text-white" to="/">Home</Link>
+                    <div className="btn-group">
+                        <button className="btn btn-primary dropdown-toggle" data-toggle="dropdown"> Categories </button>
+                        <div className="dropdown-menu">
+                            {this.state.categories.map(category => (
+                                <Link className="dropdown-item" key={category.path} to={`/categoria/${category.name}`}>{category.name}</Link>
+                            ))}
+                        </div>
+                    </div>
+                    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span className="navbar-toggler-icon"></span> </button>
+                    <div className="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
+                        <ul className="navbar-nav"></ul>
+                        <a className="btn navbar-btn ml-2 text-white btn-primary"><FANewspaper />&nbsp;New Post</a>
+                    </div>
                 </div>
             </nav>
 

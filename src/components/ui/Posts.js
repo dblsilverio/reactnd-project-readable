f9586b59b88fe441f3599184efed3e827585200b
Diogo Silvério <dblsilverio@gmail.com>
2017-12-31 16:47:17 -0200

    refactoring: improving posts listing ui

--- src/components/ui/Posts.js
+++ src/components/ui/Posts.js
@@ -7,29 +7,38 @@ export default class Posts extends Component {
 
     render() {
         return (
-            <div>
-                <h1>{this.props.nome}</h1>
-
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th style={{ width: '50%' }}>Título</th>
-                            <th>Autor</th>
-                            <th>Data Publicação</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.props.posts.map(post => (
-                                <tr key={post.id}>
-                                    <td><Pontuacao pontos={post.voteScore} /> <Link to={`/posts/${post.id}`}>{post.title}</Link></td>
-                                    <td>{post.author}</td>
-                                    <td>{new Date(post.timestamp).toLocaleDateString()}</td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
+            <div className="py-5">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="">Top Posts</h1>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th style={{ width: '50%' }}>Title</th>
+                                        <th>Author</th>
+                                        <th>Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.props.posts.map(post => (
+                                            <tr key={post.id}>
+                                                <td><Pontuacao pontos={post.voteScore} /> <Link to={`/posts/${post.id}`}>{post.title}</Link></td>
+                                                <td>{post.author}</td>
+                                                <td>{new Date(post.timestamp).toLocaleDateString()}</td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }

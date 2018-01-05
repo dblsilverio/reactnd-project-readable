c2b33bb6d754a2ac3ff83c1568df2b0fa9cf1668
Diogo Silvério <dblsilverio@gmail.com>
2018-01-05 01:29:23 -0200

    fix: new comments had undef voteScore

--- src/reducers/comment/index.js
+++ src/reducers/comment/index.js
@@ -24,7 +24,8 @@ export default function comment(state = { comments: [] }, action) {
             }
 
             updatedComments.push(comment);
-
+            comment.voteScore = 1;
+            
             return {
                 comments: updatedComments
             }
